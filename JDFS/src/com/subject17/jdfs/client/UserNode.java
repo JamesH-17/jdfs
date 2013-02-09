b0963e6255a203a6c69b21b2bd844901f6643233
JamesH-17 <james.hughes@seisan.com>
2013-02-09 17:34:07 -0500

    Introduced SettingsReader class for reading xml documents

---
+++ JDFS/src/com/subject17/jdfs/client/UserNode.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.subject17.jdfs.client;
+
+/**
+ * @author James Hughes
+ *
+ */
+public class UserNode {
+
+	/**
+	 * @param args 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
