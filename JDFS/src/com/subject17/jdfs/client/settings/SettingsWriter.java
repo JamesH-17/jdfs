f00869eef96e45ce03a7de3901d12a2a2499068a
James <james@THOR.etown.edu>
2013-02-10 14:21:08 -0500

    More work on SettingsReader, created SettingsWriter

---
+++ JDFS/src/com/subject17/jdfs/client/settings/SettingsWriter.java
@@ -0,0 +1,14 @@
+package com.subject17.jdfs.client.settings;
+
+public class SettingsWriter extends Settings {
+	
+	public void writeXMLSettings() { writeXMLSettings(settingsFileName); }
+	
+	public void writeXMLSettings(String settingsFileLocation) {
+		try {
+			
+		} catch (Exception e) {
+			
+		}
+	}
+}
