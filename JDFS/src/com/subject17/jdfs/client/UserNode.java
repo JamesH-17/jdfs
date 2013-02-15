9037695bbad3b0645daa0d49eacbe18d70c3ab05
James Hughes <james.hughes@seisan.com>
2013-02-15 16:23:12 -0500

    mergefix

--- JDFS/src/com/subject17/jdfs/client/UserNode.java
+++ JDFS/src/com/subject17/jdfs/client/UserNode.java
@@ -29,28 +29,31 @@ public class UserNode {
 		createServerListener(port);
 	}
 	
-	public static void createClientListener(int port) {
+	public static void createClientListener(int port, String serverName) {
 		try {
-			System.out.println("Please enter a servername:");
-			String serverName = input.readLine();
+			String servername = input.readLine();
 			Socket sock = new Socket(serverName, port);
 			
 			PrintWriter output = new PrintWriter(sock.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-			
+			System.out.println("Connected!");
 			String serverMsg;
-			while (!((serverMsg = in.readLine()) == null ? "" : serverMsg).equals("exit")) {
-				System.out.println(serverMsg);
+			
+			do {
+				serverMsg = in.readLine();
+				if (serverMsg!=null)
+					System.out.println(serverMsg);
 				
 				output.println(input.readLine());
-			}
+			} while (!serverMsg.equals("exit"));
+			
 
 			output.close();
 			in.close();
 			input.close();
 			sock.close();
 		} catch(IOException e){
-			System.out.println("Could not listen on port "+port);
+			System.out.println("Client Could not listen on port "+port);
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
