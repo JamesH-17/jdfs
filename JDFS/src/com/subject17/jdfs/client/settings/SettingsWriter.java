6bc8f2b1c0a74654a948db6024b0319f4c313ef7
James <james@THOR.etown.edu>
2013-02-17 20:30:32 -0500

    Refactoring, timeline established

--- JDFS/src/com/subject17/jdfs/client/settings/SettingsWriter.java
+++ JDFS/src/com/subject17/jdfs/client/settings/SettingsWriter.java
@@ -2,7 +2,7 @@ package com.subject17.jdfs.client.settings;
 
 public class SettingsWriter extends Settings {
 	
-	public void writeXMLSettings() { writeXMLSettings(settingsFileName); }
+	//public void writeXMLSettings() { writeXMLSettings(settingsFileName); }
 	
 	public void writeXMLSettings(String settingsFileLocation) {
 		try {
