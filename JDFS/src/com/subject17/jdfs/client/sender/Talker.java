81716e9ff0cc15b69bf85958d2e5049e180808a3
JamesH-17 <james.hughes@seisan.com>
2013-02-25 18:37:34 -0500

    Talker

--- JDFS/src/com/subject17/jdfs/client/sender/Talker.java
+++ JDFS/src/com/subject17/jdfs/client/sender/Talker.java
@@ -6,9 +6,12 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import com.subject17.jdfs.client.io.Printer;
+import com.subject17.jdfs.client.net.LanguageProtocol;
+
 public class Talker {
-	private static String defaultServerName = "";
-	private static int defaultPort = 0;
+	private String defaultServerName = "";
+	private int defaultPort = 0;
 	
 	protected String serverName;
 	protected int port;
@@ -18,9 +21,9 @@ public class Talker {
 	 * @param port -- The port Number that this service will listen on
 	 * @param String
 	 */
-	public Talker() { this(defaultServerName,defaultPort); }
-	public Talker(String serv) { this(serv,defaultPort); }
-	public Talker(int targetPort) { this(defaultServerName,targetPort); }
+	public Talker() {setServer(defaultServerName); setPort(defaultPort); }
+	public Talker(String serv) { setServer(serv); setPort(defaultPort); }
+	public Talker(int targetPort) { setServer(defaultServerName);setPort(targetPort); }
 	
 	public Talker(String serv, int p){
 		setServer(serv);
@@ -33,23 +36,31 @@ public class Talker {
 	public int getPort() {return port;}
 	public String getServer() {return serverName;}
 	
-	public static void createTalker(int port) {
+	public void createTalker() {
 		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			System.out.println("Please enter a servername:");
 			String serverName = userInput.readLine();
 			Socket sock = new Socket(serverName, port);
-			
+			Printer.log("Connected to server "+sock.getInetAddress());
+			Printer.log("IsInputShutDown"+sock.isInputShutdown());
+			Printer.log("IsOutputShutDown"+sock.isOutputShutdown());
 			PrintWriter output = new PrintWriter(sock.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			
-			String serverMsg;
-			while (!((serverMsg = in.readLine()) == null ? "" : serverMsg).equals("exit")) {
-				System.out.println(serverMsg);
-				
-				output.println(userInput.readLine());
-			}
-
+			//output.print
+			Printer.log("begin output");
+			output.println(LanguageProtocol.SYN);
+			Printer.log("output done");
+			//if (in.readLine().equals(LanguageProtocol.ACK)) {
+			//	
+				String msg;
+				do {
+					msg = userInput.readLine();
+					output.println(msg);
+				}
+				while(msg!=null && msg.equals("") && !msg.equals("exit"));
+			//}
 			output.close();
 			in.close();
 			userInput.close();
