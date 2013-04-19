06dc2912a1bfdc61ee8a3ec69e1f94b5774817cd
James <james.hughes@seisan.com>
2013-04-19 15:25:07 -0400

    Minor changes

--- JDFS/src/net/subject17/jdfs/client/net/sender/FileRetriever.java
+++ JDFS/src/net/subject17/jdfs/client/net/sender/FileRetriever.java
@@ -1,30 +1,37 @@
 package net.subject17.jdfs.client.net.sender;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Path;
 import java.util.UUID;
 
 import net.subject17.jdfs.client.file.FileUtil;
+import net.subject17.jdfs.client.file.handler.FileHandler;
+import net.subject17.jdfs.client.file.model.FileRetrieverRequest;
 import net.subject17.jdfs.client.io.Printer;
 
 public class FileRetriever implements Runnable {
 	public String ipAddr;
 	public int port;
 	public final Path storeLocation;
-	public final String jsonPayload;
+	//public final String jsonPayload; //FileRetrieverRequest
 	public final UUID fileToRetrieveIdentifier;
+	public final FileRetrieverRequest criteria;
 	
-	public FileRetriever(String ip, int port, Path storeLocation, UUID fileUUID, String jsonPayload) {
-		this(storeLocation, fileUUID, jsonPayload);
+	public FileRetriever(String ip, int port, Path storeLocation, UUID fileUUID, String jsonPayload, FileRetrieverRequest criteria) {
+		this(storeLocation, fileUUID, criteria);
 		this.ipAddr = ip;
 		this.port = port;
 	}
 	
-	public FileRetriever(Path storeLocation, UUID fileUUID, String jsonPayload) {
+	public FileRetriever(Path storeLocation, UUID fileUUID, String jsonPayload, FileRetrieverRequest criteria) {
 		this.fileToRetrieveIdentifier = fileUUID;
-		this.jsonPayload = jsonPayload;
+		//this.jsonPayload = jsonPayload;
 		this.storeLocation= storeLocation; 
+		this.criteria = criteria;
 	}
 	
 	public void setIP(String ip)	{ this.ipAddr = ip; }
@@ -32,13 +39,16 @@ public class FileRetriever implements Runnable {
 	
 	@Override
 	public void run() {
-		try( Socket socket = new Socket(ipAddr, port);
-			 OutputStream toServer = socket.getOutputStream()
+		try( ServerSocket servSock = new ServerSocket(port);
+			 Socket socket = servSock.accept();
+			 InputStream fromListener = socket.getInputStream()
 		 ) {
 		 	//TODO put in a guid
-			Printer.log("Sending file "+path.getFileName()+"on connection "+ipAddr+":"+port);
+			Printer.log("Recieving file "+storeLocation.getFileName()+" on connection "+ipAddr+":"+port);
 			
-			FileUtil.getInstance().readFileToStream(path, toServer);
+			//Path storageLocation = FileHandler.getInstance().getStorageLocation
+			
+			FileUtil.getInstance().readStreamToStream(fromListener, new FileOutputStream(storeLocation.toFile()));
 			
 			Printer.log("File sent!");
 	    }
